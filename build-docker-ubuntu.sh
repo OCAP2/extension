96bf95aae2d9469734171b0c5113e719745d686d
Florian Kinder <florian.kinder@enthus.de>
2023-08-19 10:30:55 +0200

    Updated cmake file to build with older version

--- build-docker-ubuntu.sh
+++ build-docker-ubuntu.sh
@@ -1,6 +1,17 @@
-docker run -v .:/build ubuntu:22.04 bash -c '\
+docker run --rm --platform linux/amd64 -it -v .:/build ubuntu:20.04 bash -c '\
   apt-get update &&\
   apt-get install -y build-essential cmake g++ libcurl4-openssl-dev zlib1g-dev &&\
-  cmake /build/OcapReplaySaver2 &&\
+  cd /build/OcapReplaySaver2 &&\
+  cmake . &&\
   make &&\
-  cp OcapReplaySaver2_x64.so /build'
+  mkdir -p /build/build/ubuntu_20.04/
+  cp OcapReplaySaver2_x64.so /build/build/ubuntu_20.04/'
+
+docker run --rm --platform linux/amd64 -it -v .:/build ubuntu:22.04 bash -c '\
+  apt-get update &&\
+  apt-get install -y build-essential cmake g++ libcurl4-openssl-dev zlib1g-dev &&\
+  cd /build/OcapReplaySaver2 &&\
+  cmake . &&\
+  make &&\
+  mkdir -p /build/build/ubuntu_22.04/
+  cp OcapReplaySaver2_x64.so /build/build/ubuntu_22.04/'
